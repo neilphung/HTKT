cee51913fe6dec9150f7f750e58b7e4e1db248fe
NeilPhung <neilphung@neilphungs-macbook-pro.local>
2019-07-16 08:44:02 +0700

    install pod Firebase/Auth and complate Register view

--- HTKT/Controller/RegisterViewController.swift
+++ HTKT/Controller/RegisterViewController.swift
@@ -7,24 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerPressed(_ sender: UIButton) {
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+        
+            if error != nil {
+                print(error!)
+            }
+            else {
+                self.performSegue(withIdentifier: "goToHome", sender: self)
+            }
+        }
     }
-    */
+    
 
 }
