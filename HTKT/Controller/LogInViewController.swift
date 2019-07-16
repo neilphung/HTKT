960bbbc0d43ab8d392564390e900ff9b55046f07
NeilPhung <neilphung@neilphungs-macbook-pro.local>
2019-07-16 08:54:44 +0700

    Complate login with email

--- HTKT/Controller/LogInViewController.swift
+++ HTKT/Controller/LogInViewController.swift
@@ -7,24 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func LogInPressed(_ sender: UIButton) {
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            }
+            else {
+               self.performSegue(withIdentifier: "goToHome", sender: self)
+            }
+        }
     }
-    */
+    
 
 }
